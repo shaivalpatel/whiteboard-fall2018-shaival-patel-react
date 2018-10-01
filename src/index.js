83708499de9d1339e7d074dfc830a50ddaa4bb51
shaivalpatel <patel.shaiv@husky.neu.edu>
2018-10-01 08:52:33 -0400

    adding index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import HelloWorld from './components/HelloWorld'
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/font-awesome/css/font-awesome.css';
+import WhiteBoard from './containers/WhiteBoard'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+
+
+
+ReactDOM.render(
+    <div className="container-fluid">
+        <WhiteBoard/>
+
+    </div>,
+    document.getElementById("root")
+)
