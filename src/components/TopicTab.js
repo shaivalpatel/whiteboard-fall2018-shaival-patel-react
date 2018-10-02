3ba954c51f2bff2a4cfdab1302c8fa8ed0267a54
shaivalpatel <patel.shaiv@husky.neu.edu>
2018-10-01 23:38:22 -0400

    adding topic tabs

---
+++ src/components/TopicTab.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
+
+const TopicTab = ({topic, selectTopic, selected,deleteTopic,getTopicName,}) =>
+    <li className="nav-item">
+        <a onClick={() => selectTopic(topic)}
+           className={selected ? "nav-link active" : "nav-link"}>
+            {topic.title}
+            <a onClick= {()=> getTopicName(topic)}>
+                <i className="fa fa-pencil"></i></a>
+            <a onClick= {()=>deleteTopic(topic)}>
+            <i className="fa fa-times"></i></a>
+        </a>
+    </li>
+
+export default TopicTab
---
+++ src/components/TopicTabs.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import TopicTab from "./TopicTab";
+
+const TopicTabs = ({topics, selectTopic, selectedTopic,deleteTopic,getTopicName ,topicChanged,addNewTopic}) =>
+    <ul className="nav nav-tabs">
+
+        {
+            topics.map((topic, index) =>
+                <TopicTab
+                    selected={selectedTopic === topic}
+                    selectTopic={selectTopic}
+                    topic={topic}
+                    key={index}
+                    topicChanged={this.topicChanged}
+                    addNewTopic={this.addNewTopic}
+                    deleteTopic={deleteTopic}
+                getTopicName={getTopicName}/>
+            )
+        }
+        <li>
+            <input onChange={topicChanged} className="form-control"/>
+            <button onClick={addNewTopic} className="btn btn-primary">Add</button>
+        </li>
+    </ul>
+
+export default TopicTabs
