700bb56a7fc302e4a78bf7e014661fbfce511bb9
shaivalpatel <patel.shaiv@husky.neu.edu>
2018-10-14 23:10:11 -0400

    updating components

--- src/components/WidgetList.js
+++ src/components/WidgetList.js
@@ -1,71 +1,109 @@
 import React from 'react'
+import HeadingWidget from "./HeadingWidget";
+import ListWidget from "./ListWidget";
+import LinkWidget from "./LinkWidget";
+import ParagraphWidget from "./ParagraphWidget";
+import ImageWidget from "./ImageWidget";
+import ToggleButton from 'react-toggle-button'
+import CourseServiceSingleton from "../services/CourseServiceSingleton";
 
-const WidgetList=()=>
+class WidgetList extends React.Component {
 
-<div>
-<div class="row">
-    <div class="col-sm-8 ">
-    &nbsp
-    </div>
-<div class="col-sm-4">
-    <div class="float-right">
-        <button  class="btn btn-success ">Save</button>
-        Preview
-        <button class="btn btn-white" >
-            <i class="fa fa-toggle-on fa-2x"></i>
-        </button>
+    constructor(props) {
 
-    </div>
-</div>
-</div>
-    <ul className="list-group">
-    <li className="list-group-item">
-        <div className="row">
-            <div className="col-sm-8">
-                <h1>Heading Widget</h1>
-            </div>
-            <div className="col-sm-4 ">
-                <div className="float-right">
-                    <button className="btn bg-warning arrowbtn"><i className="fa fa-arrow-up"></i></button>
-                    <button className="btn bg-warning arrowbtn"><i className="fa fa-arrow-down"></i></button>
-                    <select>
-                        <option value="Heading">Heading</option>
-                        <option value="Heading">Paragraph</option>
-                        <option value="Heading">List</option>
-                        <option value="Heading">Image</option>
-                        <option value="Heading">Link</option>
-                    </select>
-                    <button className="btn btn-danger"><i className="fa fa-times"></i></button>
+
+        super(props);
+        let c;
+        const isLoggedIn = this.props.widgets.length==1;
+        props.init(props.widgetsInit, props.topic)
+
+
+    }
+
+    componentDidUpdate() {
+
+        this.props.init(this.props.widgetsInit, this.props.topic)
+    }
+    render() {
+
+        let c;
+        let defaultwidget={
+            id: '456',
+            title: 'list 1',
+            type: 'HEADING',
+            size:1,
+            text:"hello"
+
+        };
+        return(
+            <div >
+                <div className="row">
+                    <div className="col-sm-8 ">
+                        &nbsp
+                    </div>
+                    <div className="col-sm-4">
+                        <div className="float-right">
+                            <button onClick={this.props.saveWidget} className="btn btn-success ">Save</button>
+                            Preview
+                            <ToggleButton value={this.props.preview}
+                                          onToggle={this.props.changePreview}/>
+
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-        <div>
-            <div className="form-group row">
-                <input type="text" placeholder="Heading Text" className="form-control"/>
-            </div>
-            <div className="form-group row">
-                <select className="form-control">
-                    <option value="heading 1">Heading 1</option>
-                    <option value="heading 1">Heading 2</option>
-                    <option value="heading 1">Heading 3</option>
-                    <option value="heading 1">Heading 4</option>
-                    <option value="heading 1">Heading 5</option>
-                    <option value="heading 1">Heading 6</option>
-                </select>
-            </div>
-            <div className="form-group row">
-                <input type="text" placeholder="Widget Name" className="form-control"/>
-            </div>
-            <div>
-                <h3>Preview</h3>
-                Heading text
-            </div>
-        </div>
-    </li>
-    </ul>
-</div>
+            <ul className="list-group">
+                {
+
+                    this.props.widgets.map((widget, index) =>
+                        <li key={index}
+                            className="list-group-item">
+                            {}
+                            <button hidden={(index==0)} onClick={()=>this.props.moveUp(widget,index)} className="btn bg-warning arrowbtn float-right"><i className="fa fa-arrow-up"></i></button>
+                            <button hidden={index==this.props.widgets.length-1} onClick={()=>this.props.moveDown(widget,index)}className="btn bg-warning arrowbtn float-right"><i className="fa fa-arrow-down"></i></button>
+                            <select className="float-right"
+                                    onChange={(event)=>
+                                                {widget.type=event.target.value;
+                                                this.props.updateWidget(widget);}}
+                                    value={widget.type}>
+
+                                <option  value="HEADING">Heading</option>
+                                <option  value="PARAGRAPH">Paragraph</option>
+                                <option  value="LIST">List</option>
+                                <option  value="IMAGE">Image</option>
+                                <option  value="LINK">Link</option>
+                            </select>
+                            <button onClick={() => this.props.deleteWidget(widget)}
+                                    className="btn btn-danger float-right"><i className="fa fa-times"></i></button>
+
+                            {widget.type === 'LIST' &&
+                            <ListWidget preview={this.props.preview} widget={widget}
+                                        updateWidget={this.props.updateWidget}/>}
+                            {widget.type === 'HEADING' &&
+                            <HeadingWidget preview={this.props.preview} widget={widget}
+                                           updateWidget={this.props.updateWidget}/>}
+                            {widget.type === 'LINK' &&
+                            <LinkWidget preview={this.props.preview} widget={widget}
+                                           updateWidget={this.props.updateWidget}/>}
+                            {widget.type === 'PARAGRAPH' &&
+                            <ParagraphWidget preview={this.props.preview} widget={widget}
+                                        updateWidget={this.props.updateWidget}/>}
+                            {widget.type === 'IMAGE' &&
+                            <ImageWidget preview={this.props.preview} widget={widget}
+                                             updateWidget={this.props.updateWidget}/>}
 
 
+                        </li>
+                    )
 
-
+                }
+            </ul>
+                <div className="text-danger float-right">
+                <button onClick={()=>this.props.createWidget(defaultwidget)} >
+                   Add
+                </button>
+            </div>
+               </div>
+        )
+    }
+}
 export default WidgetList
