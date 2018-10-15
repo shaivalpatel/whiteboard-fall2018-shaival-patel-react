fb598c12c1848eccc58e84f50c2270fda8254d79
shaivalpatel <patel.shaiv@husky.neu.edu>
2018-10-15 16:50:28 -0400

    image widget preview handling

--- src/components/ImageWidget.js
+++ src/components/ImageWidget.js
@@ -1,20 +1,20 @@
 import React from 'react'
 
-const ImageWidget = ({widget, updateWidget}) => {
+const ImageWidget = ({preview,widget,updateWidget}) => {
 
     return(
         <div>
             <h1>{widget.title}</h1>
 
             <div>
-                <div className="form-group row">
+                <div hidden={preview} className="form-group row">
                     <input onChange= {(event)=>
                     {widget.link=event.target.value;
                         updateWidget(widget);}}type="text"
                            placeholder="https://techcrunch.com/wp-content/uploads/2018/03/gettyimages-705351545.jpg?w=730&crop=1"
                            className="form-control"/>
                 </div>
-                <div className="form-group row">
+                <div hidden={preview} className="form-group row">
                     <input onChange= {(event)=>
                     {widget.title=event.target.value;
                         updateWidget(widget);}} type="text" placeholder="Widget Name" className="form-control"/>
